1e15109cdca1aa4600af7006d2ccf84f6f99c55d
Rameswara <konakalla.ramesh143@gmail.com>
2025-10-29 14:14:35 +0530

    Updated code working in my local

--- canteen-project/backend/src/main/java/com/canteen/entity/User.java
+++ canteen-project/backend/src/main/java/com/canteen/entity/User.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
